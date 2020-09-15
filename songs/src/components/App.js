f9dd19c2e79fc05eaddcd7e25b01c0a618c7e47b
Personal <jordan_munk@hotmail.com>
2020-09-15 10:32:15 +0200

    React songs afronden

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import { selectSong } from '../actions';
+import SongList from './SongList.js';
+import SongDetail from './SongDetail.js';
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className="ui container grid">
+            <div className="ui row">
+                <div className="column eight wide">
+                    <SongList />
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
+                </div>
+            </div>
+        </div>
     );
 };
 
