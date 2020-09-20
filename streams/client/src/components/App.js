831a663dfb89d8fc3968436b9fc3ccc029ecd8ca
jor <jor>
2020-09-20 22:09:00 +0200

    Api/client twitch clone

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route, Link } from 'react-router-dom';
 
-const PageOne = () => {
-    return <div>PageOne</div>
-};
-const PageTwo = () => {
-    return <div>PageTwo</div>
-};
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+import history from '../history';
 
 const App = () => {
     return (
-        <div>
-            <BrowserRouter>
+        <div className="ui container">
+            <Router history={history}>
                 <div>
-                    <Route path="/" exact component={PageOne} />
-                    <Route path="/pagetwo" component={PageTwo} />
+                    <Header />
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                    <Route path="/streams/show" exact component={StreamShow} />
                 </div>
-            </BrowserRouter>
+            </Router>
         </div>
     );
 };
